60355501c4bf3a1fba074c6f0e22edacd3bdc54c
Your Name <you@example.com>
2020-03-05 14:23:58 +0800

    前台获取全部文章请求

--- src/pages/PostList.vue
+++ src/pages/PostList.vue
@@ -1,12 +1,36 @@
 <template>
-    <div>PostList</div>
+     <div>文章列表</div>
+
 </template>
 
 <script>
     export default {
-        name: "PostList"
+        name: "PostList",
+        methods:{
+          getPostList(){
+            this.$axios(
+              {
+                url:'/get_posts',
+                method:'get',
+                params:{
+                  pageIndex:2,
+                  pageSize:5
+                }
+              }
+            ).then(res=>{
+                console.log(res.data.data)
+              }
+            )
+          }
+        },
+        mounted(){
+          this.getPostList()
+        }
     }
 </script>
+
+<style scoped>
+</style>
 
 <style scoped>
 
